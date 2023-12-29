97381ab56051fce03f89d47f2bb384d6e6cced95
Ferminm99 <ferminmorenoz99@gmail.com>
2023-12-29 19:58:21 -0300

    'arreglo'

--- db/migrate/20231130145220_add_fields_to_links.rb
+++ db/migrate/20231130145220_add_fields_to_links.rb
@@ -2,9 +2,10 @@ class AddFieldsToLinks < ActiveRecord::Migration[7.1]
   def change
     add_column :links, :slug, :string, null: false
     add_index :links, :slug, unique: true
-    add_column :links, :link_type, :string, null: false
+    add_column :links, :link_type, :integer, null: false
     add_column :links, :password_digest, :string
     add_column :links, :expiration_date, :datetime
     add_column :links, :access_count, :integer, default: 0
   end
 end
+
